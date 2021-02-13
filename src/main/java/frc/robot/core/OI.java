89dfc173bed10c84cd9612e861acab36c45f89b5
etc3po <46801109+etc3po@users.noreply.github.com>
2021-02-13 11:44:54 -0700

    Mapping the gamepad

--- src/main/java/frc/robot/core/OI.java
+++ src/main/java/frc/robot/core/OI.java
@@ -1,7 +1,12 @@
 package frc.robot.core;
+import edu.wpi.first.wpilibj.Joystick;
+
+
 public final class OI {
     //init all joystics, motor controllers, and io devices here
+    public Joystick gamepad;
     public OI(){
         //assign values here
+        gamepad = new Joystick(robotMap.gamepad);
     }
 }
